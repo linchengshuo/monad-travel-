655db94067d588b505fa326f6a72050a6c0935b8
林诚朔 <144406999+linchengshuo@users.noreply.github.com>
2026-05-31 02:14:54 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite"; // 从 Vite 里拿到定义配置的函数。
-import react from "@vitejs/plugin-react"; // 引入 React 插件，让 Vite 能理解 React 代码。
-
-export default defineConfig({ // 导出 Vite 配置，Vite 启动时会读取这里。
-  plugins: [react()], // 启用 React 插件，支持 JSX 和快速刷新。
-}); // 配置对象到这里结束。
